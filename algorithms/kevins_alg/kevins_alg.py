62bc29052506d9d44ae64a70f0ef64f94416afaa
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-01-17 18:46:02 -0500

    Create new algorithm

---
+++ algorithms/kevins_alg/kevins_alg.py
@@ -0,0 +1,20 @@
+from algorithms.algorithm import Algorithm
+
+# > python3 main.py node_defs.csv raw_data.csv kevins_alg
+
+
+class kevins_alg(Algorithm):
+    def __init__(self, node_arr):
+        super().__init__(node_arr)
+
+    def process(self, callback, canvas):
+        # Example direct canvas interaction
+        canvas.create_circle(100, 100, 10, fill="red")
+
+        # Print each measurement from the nodes
+        for key,node in self.node_arr.items():
+            for m in node.get_measurements():
+                print(m)
+
+        # Done!
+        callback()
